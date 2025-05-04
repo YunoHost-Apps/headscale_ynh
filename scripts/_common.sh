4e5fee127f70d68bd699aaaf553b4f8e047eedfe
tituspijean <tituspijean@yunohost.org>
2025-05-04 17:08:49 +0200

    Fix dex settings

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -26,12 +26,14 @@ setup_dex() {
 	dex_install_dir="$(ynh_app_setting_get --app $dex --key install_dir)"
 	dex_domain="$(ynh_app_setting_get --app $dex --key domain)"
 	dex_path="$(ynh_app_setting_get --app $dex --key path)"
+	oidc_name="$app"
+	oidc_callback="$domain${path%/}/oidc/callback"
 
 	ynh_app_setting_set         --key=dex_install_dir --value="$dex_install_dir"
 	ynh_app_setting_set         --key=dex_domain      --value="$dex_domain"
 	ynh_app_setting_set         --key=dex_path        --value="$dex_path"
-	ynh_app_setting_set_default --key=oidc_name       --value="$app"
-	ynh_app_setting_set_default --key=oidc_callback   --value="$dex_domain${dex_path%/}/oidc/callback"
+	ynh_app_setting_set         --key=oidc_name       --value="$oidc_name"
+	ynh_app_setting_set         --key=oidc_callback   --value="$oidc_callback"
 	ynh_app_setting_set_default --key=oidc_secret     --value="$(ynh_string_random --length=32 --filter='A-F0-9')"
 
 	bash "$dex_install_dir/add_config.sh" $app $oidc_name $oidc_callback $oidc_secret
