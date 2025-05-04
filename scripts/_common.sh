6d01e0c1be4e60bd019750d0ffe1237887da5db2
tituspijean <tituspijean@yunohost.org>
2025-05-04 11:17:46 +0200

    Fix and improve setup_dex function

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,13 +5,18 @@
 #=================================================
 
 setup_dex() {
-	if [ $(dpkg --compare-versions "$(yunohost app info dex --output-as json | jq -r '.version')" lt "v2.42.1~ynh4") ]; then
+	dex_version=$(yunohost app info dex --output-as json | jq -r '.version')
+	if [ $(dpkg --compare-versions "${dex_version#v}" lt "2.42.1~ynh4") ]; then
 		ynh_die "You need to upgrade Dex to v2.42.1~ynh4 and above first."
 	fi
 
-	ynh_app_setting_set_default --key=dex_install_dir --value="$(ynh_app_setting_get --app $dex --key install_dir)"
-	ynh_app_setting_set_default --key=dex_domain      --value="$(ynh_app_setting_get --app $dex --key domain)"
-	ynh_app_setting_set_default --key=dex_path        --value="$(ynh_app_setting_get --app $dex --key path)"
+	dex_install_dir="$(ynh_app_setting_get --app $dex --key install_dir)"
+	dex_domain="$(ynh_app_setting_get --app $dex --key domain)"
+	dex_path="$(ynh_app_setting_get --app $dex --key path)"
+
+	ynh_app_setting_set         --key=dex_install_dir --value="$dex_install_dir"
+	ynh_app_setting_set         --key=dex_domain      --value="$dex_domain"
+	ynh_app_setting_set         --key=dex_path        --value="$dex_path"
 	ynh_app_setting_set_default --key=oidc_name       --value="$app"
 	ynh_app_setting_set_default --key=oidc_callback   --value="$dex_domain${dex_path%/}/oidc/callback"
 	ynh_app_setting_set_default --key=oidc_secret     --value="$(ynh_string_random --length=32 --filter='A-F0-9')"
