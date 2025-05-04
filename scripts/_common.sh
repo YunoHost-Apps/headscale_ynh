51604b928c8663bb18f84748a530d30c9a059a05
tituspijean <tituspijean@yunohost.org>
2025-05-04 12:57:41 +0200

    Improve Dex app handling

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,19 @@
 #=================================================
 
 setup_dex() {
+	dex_apps="$(yunohost app list -f --output-as json | jq -r '[ .apps[] | select(.manifest.id == "dex") ]')"
+	first_dex="$(jq -r 'sort_by(.id) | first.id' <<< $dex_apps)"
+
+	if [ $(jq -r '[ .[] | select(.manifest.id == "dex").id ] | length' <<< $dex_apps) -eq 0 ]
+	then
+	    ynh_die "The apps needs at least one Dex instance to be installed. Install or restore one first."
+	elif [ $first_dex != $dex ]
+	then
+	    ynh_print_warn "The $dex app initially set up for $app has not been found. Reconfiguring with $first_dex"
+	    dex=$first_dex
+	    ynh_app_setting_set --key=dex --value=$dex
+	fi
+
 	dex_version=$(yunohost app info dex --output-as json | jq -r '.version')
 	if [ $(dpkg --compare-versions "${dex_version#v}" lt "2.42.1~ynh4") ]; then
 		ynh_die "You need to upgrade Dex to v2.42.1~ynh4 and above first."
